a3358865fb850e4bebf7fb6256bbfd4d03312b3b
Jon <jonlz@github.com>
2015-10-07 16:32:53 -0400

    Finished required tasks

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,17 +11,48 @@ import UIKit
 class ViewController: UIViewController
 {
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
    
     var userIsInTheMiddleOfTypingANumber: Bool = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
+        appendToDisplay(digit)
+    }
+    
+    @IBAction func decimal(sender: UIButton) {
+        let decimalFound = display.text!.rangeOfString(".")
+        if (decimalFound == nil || !userIsInTheMiddleOfTypingANumber)
+        {
+            if (!userIsInTheMiddleOfTypingANumber)
+            {
+                appendToDisplay("0.")
+            } else {
+                appendToDisplay(".")
+            }
+        }
+    }
+    
+    @IBAction func pi() {
+        userIsInTheMiddleOfTypingANumber = false
+        displayValue = 3.14159265359
+        enter()
+    }
+    
+    @IBAction func clear() {
+        operandStack.removeAll()
+        displayValue = 0
+        history.text = ""
+    }
+    
+    func appendToDisplay(name:String) {
         if userIsInTheMiddleOfTypingANumber {
-            display.text = display.text! + digit
+            display.text = display.text! + name
         } else {
-            display.text = digit
+            display.text = name
             userIsInTheMiddleOfTypingANumber = true
         }
+        history.text = history.text! + name
     }
     
     @IBAction func operate(sender: UIButton) {
@@ -29,13 +60,16 @@ class ViewController: UIViewController
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
+        history.text = history.text! + operation
         
         switch operation {
             case "×": performOperation() { $0 * $1}
             case "÷": performOperation() { $1 / $0}
             case "+": performOperation() { $0 + $1}
             case "-": performOperation() { $1 - $0}
-            case "√": performOperation() { sqrt($0) }
+            case "√": performOperation() { sqrt($0)}
+            case "sin" : performOperation() { sin($0)}
+            case "cos" : performOperation() { cos($0)}
             default: break
         }
     }
@@ -59,6 +93,7 @@ class ViewController: UIViewController
     
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
+        history.text = history.text! + "⏎"
         operandStack.append(displayValue)
         print("operandStack = \(operandStack)")
     }
@@ -72,6 +107,5 @@ class ViewController: UIViewController
             userIsInTheMiddleOfTypingANumber = false
         }
     }
-    
 }
 
